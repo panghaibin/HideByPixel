f33bd49830720922cae38df0fd9c90083f5b4b27
panghaibin <63835145+panghaibin@users.noreply.github.com>
2021-01-24 23:45:30 +0800

    删除多余注释

--- web.py
+++ web.py
@@ -126,8 +126,6 @@ def readImg(filename):
         return "未读取到结果"
 
 
-# importlib.reload(sys)
-# sys.setdefaultencoding('utf-8')
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
